82dda52af70993e1c9acfbe1e2aa27487b787c6a
NazarHussain12 <151500841+Nazarhussain12@users.noreply.github.com>
2024-02-21 22:19:27 +0500

    Created new component for dashboard

--- Web_frontend/src/App.js
+++ Web_frontend/src/App.js
@@ -3,15 +3,19 @@ import LoginSignup from './components/LoginSignup/LoginSignup';
 import NextPage from './components/NextPage/NextPage';
 
 import {BrowserRouter, Routes, Route} from "react-router-dom"
+// App.js
+import Dashboard from './components/Dashboard/Dashboard'; // Import the Dashboard component
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/LoginSignup' element={<LoginSignup />}></Route>
-        <Route path='/NextPage' element={<NextPage/>}></Route>
+        <Route path='/' element={<Dashboard />}></Route> {/* Dashboard as the root or another path */}
       </Routes>
     </BrowserRouter>
   );
 }
+
 
 export default App;
