32935c732316053906a6a02a3e13b37f1314029b
NazarHussain12 <151500841+Nazarhussain12@users.noreply.github.com>
2024-02-22 22:47:49 +0500

    default text

--- Web_frontend/src/components/Tabs/Tabs.jsx
+++ Web_frontend/src/components/Tabs/Tabs.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as Tabs from '@radix-ui/react-tabs';
 import { FiInfo, FiCamera } from 'react-icons/fi';
 import './styles.css';
@@ -9,22 +9,33 @@ const TabPanelContent = ({ value, children }) => (
   </Tabs.Content>
 );
 
-const TabsDemo = ({ selectedMarker }) => (
-  <Tabs.Root className="TabsRoot" defaultValue="tab1">
-    <Tabs.List className="TabsList" aria-label="Manage your account">
-      <Tabs.Trigger className="TabsTrigger" value="tab1">
-        <FiInfo className="TabIcon" />
-        Info
-      </Tabs.Trigger>
-      <Tabs.Trigger className="TabsTrigger" value="tab2">
-        <FiCamera className="TabIcon" />
-        Cameras & Videos
-      </Tabs.Trigger>
-    </Tabs.List>
-    <TabPanelContent value="tab1">
-      {selectedMarker && (
-        <div>
-          <h3>Event ID: {selectedMarker.event_id}</h3>
+const TabsDemo = ({ selectedMarker }) => {
+  const [defaultText, setDefaultText] = useState('Select a marker to view details.');
+
+  const handleMarkerSelect = (marker) => {
+    if (marker) {
+      setDefaultText('');
+    } else {
+      setDefaultText('Select a marker to view details.');
+    }
+  };
+
+  return (
+    <Tabs.Root className="TabsRoot" defaultValue="tab1">
+      <Tabs.List className="TabsList" aria-label="Manage your account">
+        <Tabs.Trigger className="TabsTrigger" value="tab1">
+          <FiInfo className="TabIcon" />
+          Info
+        </Tabs.Trigger>
+        <Tabs.Trigger className="TabsTrigger" value="tab2">
+          <FiCamera className="TabIcon" />
+          Cameras & Videos
+        </Tabs.Trigger>
+      </Tabs.List>
+      <TabPanelContent value="tab1">
+        {selectedMarker ? (
+          <div>
+             <h3>Event ID: {selectedMarker.event_id}</h3>
           <p> <b>Latitude:</b> {selectedMarker.lat}</p>
           <p><b>Longitude:</b> {selectedMarker.lon}</p>
           <p> <b>Height:</b> {selectedMarker.height}</p>
@@ -33,14 +44,17 @@ const TabsDemo = ({ selectedMarker }) => (
           <p><b>Event start since:</b> {selectedMarker.event_start_since}</p>
           <p><b>Event last seen:</b> {selectedMarker.event_last_seen}</p>
           
-          {/* Add more information or interactive elements here as needed */}
-        </div>
-      )}
-    </TabPanelContent>
-    <TabPanelContent value="tab2">
-      {/* Tab 2 content */}
-    </TabPanelContent>
-  </Tabs.Root>
-);
+            {/* Add more information or interactive elements here as needed */}
+          </div>
+        ) : (
+          <p>{defaultText}</p>
+        )}
+      </TabPanelContent>
+      <TabPanelContent value="tab2">
+        {/* Tab 2 content */}
+      </TabPanelContent>
+    </Tabs.Root>
+  );
+};
 
 export default TabsDemo;
