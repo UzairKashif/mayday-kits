4d0768c216811e534a16021bf9c7ee8ed0e19449
NazarHussain12 <151500841+Nazarhussain12@users.noreply.github.com>
2024-03-08 16:58:06 +0500

    clustered numbers

--- Web_frontend/src/components/Tabs/firms.jsx
+++ Web_frontend/src/components/Tabs/firms.jsx
@@ -56,46 +56,79 @@ const FireMap = ({ showURT, setShowURT, showNRT, setShowNRT, mapRef }) => {
     }
 
     if (showNRT) {
-      const nrtGeoJSON = createGeoJSON(nrtData);
-      if (map.getSource('nrt-data')) {
-        map.getSource('nrt-data').setData(nrtGeoJSON);
+        const nrtGeoJSON = createGeoJSON(nrtData);
+        if (map.getSource('nrt-data')) {
+          map.getSource('nrt-data').setData(nrtGeoJSON);
+        } else {
+          map.addSource('nrt-data', {
+            type: 'geojson',
+            data: nrtGeoJSON,
+            cluster: true,
+            clusterMaxZoom: 14, // Max zoom to cluster points on
+            clusterRadius: 50    // Radius of each cluster when clustering points
+          });
+    
+          // Add a layer for the clusters
+          map.addLayer({
+            id: 'nrt-clusters',
+            type: 'circle',
+            source: 'nrt-data',
+            filter: ['has', 'point_count'],
+            paint: {
+              // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+              // with three steps to implement three types of circles:
+              //   * Blue, 20px circles when point count is less than 100
+              //   * Yellow, 30px circles when point count is between 100 and 750
+              //   * Pink, 40px circles when point count is greater than or equal to 750
+              'circle-color': [
+                'step',
+                ['get', 'point_count'],
+                '#51bbd6',
+                100,
+                '#f1f075',
+                750,
+                '#f28cb1'
+              ],
+              'circle-radius': [
+                'step',
+                ['get', 'point_count'],
+                20,
+                100,
+                30,
+                750,
+                40
+              ]
+            }
+          });
+    
+          // Add a layer for the cluster counts
+          map.addLayer({
+            id: 'nrt-cluster-count',
+            type: 'symbol',
+            source: 'nrt-data',
+            filter: ['has', 'point_count'],
+            layout: {
+              'text-field': '{point_count_abbreviated}', // This will display the number of points in the cluster
+              'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+              'text-size': 12
+            }
+          });
+    
+          // Add a layer for the unclustered points
+          map.addLayer({
+            id: 'nrt-unclustered-point',
+            type: 'circle',
+            source: 'nrt-data',
+            filter: ['!', ['has', 'point_count']],
+            paint: {
+              'circle-color': '#11b4da',
+              'circle-radius': 4,
+              'circle-stroke-width': 1,
+              'circle-stroke-color': '#fff'
+            }
+          });
+        }
       } else {
-        map.addSource('nrt-data', {
-          type: 'geojson',
-          data: nrtGeoJSON,
-          cluster: true,
-          clusterMaxZoom: 14,
-          clusterRadius: 50
-        });
-        map.addLayer({
-          id: 'nrt-clusters',
-          type: 'circle',
-          source: 'nrt-data',
-          filter: ['has', 'point_count'],
-          paint: {
-            'circle-color': [
-              'step',
-              ['get', 'point_count'],
-              '#51bbd6',
-              100,
-              '#f1f075',
-              750,
-              '#f28cb1'
-            ],
-            'circle-radius': [
-              'step',
-              ['get', 'point_count'],
-              20,
-              100,
-              30,
-              750,
-              40
-            ]
-          }
-        });
-        // Additional layers for clusters can be added here as needed.
-      }
-    } else {
       if (map.getLayer('nrt-clusters')) {
         map.removeLayer('nrt-clusters');
         map.removeSource('nrt-data');
