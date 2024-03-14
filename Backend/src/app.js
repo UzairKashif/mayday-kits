a10fa9cfd3d2445ddb6d591b23304de61424be4d
Ali Hamza <ali946484@gmail.com>
2024-03-14 12:37:39 +0500

    Revert "firms api"
    This reverts commit 5f853e4ceb5f1ca7c37a635db9c86f53096103d5.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,7 +4,6 @@ const eventRoutes = require('./routes/fireEventRoutes');
 const cors = require('cors');
 const earthquakeRoutes = require('./routes/earthquakeEvent');
 const weatherRoutes = require('./routes/weatherEvent');
-const firmRoutes = require('./routes/firmEventRoutes')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,7 +14,6 @@ app.use(cors());
 app.use('/api', eventRoutes);
 app.use('/api', earthquakeRoutes);
 app.use('/api', weatherRoutes);
-app.use('/api',firmRoutes)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
--- Backend/src/routes/firmEventRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const EventController = require('../controllers/FirmEventController');
-
-router.get('/firms-events', EventController.getEvents);
-
-module.exports = router;
